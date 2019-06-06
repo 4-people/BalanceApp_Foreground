b92e2d07c003d2ccac64ebf87b6735b5c48267c7
kjiy <eoaal2@naver.com>
2019-06-06 13:55:15 +0900

    지영 리스트 바꿈

--- app/src/main/java/com/example/GoogleMapAPI/GoogleMapActivity.java
+++ app/src/main/java/com/example/GoogleMapAPI/GoogleMapActivity.java
@@ -84,7 +84,7 @@ public class GoogleMapActivity extends AppCompatActivity
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener,
         PlacesListener,
-        GoogleMap.OnMarkerClickListener {
+        GoogleMap.OnMarkerClickListener{
 
     private GoogleApiClient mGoogleApiClient = null;
     private GoogleMap mGoogleMap = null;
@@ -136,11 +136,19 @@ public class GoogleMapActivity extends AppCompatActivity
             }
         });
 
-        Button button2 = (Button) findViewById(R.id.button2);
+        /*Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showAlertDialog();
+            }
+        });*/
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
             }
         });
 
@@ -159,9 +167,20 @@ public class GoogleMapActivity extends AppCompatActivity
 
         Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         int width = (int) (display.getWidth()); //Display 사이즈의 95%
-        int height = (int) (display.getHeight()*0.98);  //Display 사이즈의 95%
+        int height = (int) (display.getHeight());  //Display 사이즈의 95%
         getWindow().getAttributes().width = width;
         getWindow().getAttributes().height = height;
+
+        /*mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                String text = "[장시간 클릭시 이벤트] latitude ="
+                        + latLng.latitude + ", longitude ="
+                        + latLng.longitude;
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });*/
     }
 
     @Override
@@ -264,7 +283,7 @@ public class GoogleMapActivity extends AppCompatActivity
 
         setCurrentLocation(location, markerTitle, markerSnippet);   //마커 생성
         //mCurrentLocatiion = location;
-        if(previousPosition == null){  //최초 실행했을 때 (앱을 아예 처음 켰을 때??)  //이 때 가까운 place들이 뜸 처음에만!  //이거 왜 처음에 안 뜨지..?상관은 없지만
+       /* if(previousPosition == null){  //최초 실행했을 때 (앱을 아예 처음 켰을 때??)  //이 때 가까운 place들이 뜸 처음에만!  //이거 왜 처음에 안 뜨지..?상관은 없지만
             System.out.print("이전 위치가 없는 상황입니다.: ");
             previousPosition = currentPosition;
             showPlaceInformation(currentPosition);
@@ -286,7 +305,7 @@ public class GoogleMapActivity extends AppCompatActivity
                     System.out.println("비슷한 위치에 있습니다.");
                 }
             }
-        }
+        }*/
     }
 
     @Override
@@ -649,10 +668,86 @@ public class GoogleMapActivity extends AppCompatActivity
                     MarkerList.add(tmplist);  //리스트뷰에 띄울 리스트 생성
                     sort_mlist.add(m);
                 }
+                //리스트뷰 생성
+                final ListView listView = (ListView) findViewById(R.id.listView);
+                MarkerList = new ArrayList<>();
+                SimpleAdapter simpleAdapter = new SimpleAdapter(GoogleMapActivity.this, MarkerList, android.R.layout.simple_list_item_2, new String[]{"place_name", "place_type"}, new int[]{android.R.id.text1, android.R.id.text2});
+
+
+                for (MarkerInfo m : mlist) {   //이 때 거리 순으로 정렬
+                    HashMap<String, String> tmplist = new HashMap<>();
+                    tmplist.put("place_name", m.getPlaceName());
+                    tmplist.put("place_type", m.getPlaceType());
+                    if (m.getDistance() > 30) {   //시청과 범위 벗어나는 장소를 리스트에 띄우지 않기 위해서
+                        continue;
+                    }
+                    MarkerList.add(tmplist);  //리스트뷰에 띄울 리스트 생성
+                    sort_mlist.add(m);
+                }
+                listView.setAdapter(simpleAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {  //db에 저장
+                    @Override
+                    public void onItemClick(AdapterView parent, View v, int position, long id) {   //리스트를 눌렀을 때  //여기서 DB로 저장
+                        TextView tmp = (TextView) v.findViewById(android.R.id.text1);
+                        TextView tmp2 = (TextView) v.findViewById(android.R.id.text2);
+                        String title = tmp.getText().toString();
+                        String type = tmp2.getText().toString();
+                        System.out.println("클릭한 리스트에 장소 이름: " + title + "\n클릭한 리스트에 타입: " + type);
+
+                        long now = System.currentTimeMillis();
+                        Date date = new Date(now);
+                        // 시간을 나타냇 포맷을 정한다 ( yyyy/MM/dd 같은 형태로 변형 가능 )
+                        SimpleDateFormat sdfdate = new SimpleDateFormat("yyyy/MM/dd");
+                        SimpleDateFormat sdftime = new SimpleDateFormat("HH:mm:ss");
+                        final String formatDate = sdfdate.format(date);
+                        final String formatTime = sdftime.format(date);
+
+                        //db 삽입 부분
+                        DBHelper helper = new DBHelper(getApplicationContext());
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        Cursor cursor = db.rawQuery("select * from tb_timeline where endtime is NULL", null);
+
+                        //아예 처음시작하는 활동인 경우
+                        if (cursor.getCount() == 0 ) {
+                            db.execSQL("insert into tb_timeline (date, place, category, starttime) values (?,?,?,?)",
+                                    new String[]{formatDate, title, type, formatTime});
+                            return;
+                        }
+                        //전의 활동이 있는경우
+                        else {
+                            cursor.moveToFirst();
+                            String place = cursor.getString(cursor.getColumnIndex("place"));
+                            String category = cursor.getString(cursor.getColumnIndex("category"));
+                            String starttime = cursor.getString(cursor.getColumnIndex("starttime"));
+
+                            try {
+                                //활동 중 날짜가 넘어가는 경우 ///단 한 활동이 24시간을 넘지 않는다는 가정하에
+                                if (sdftime.parse(starttime).getTime() > sdftime.parse(formatTime).getTime()) {
+                                    db.execSQL("update tb_timeline set endtime=? where endtime is NULL",
+                                            new String[]{"24:00:00"});
+                                    db.execSQL("insert into tb_timeline (date, place, category, starttime) values (?,?,?,?)",
+                                            new String[]{formatDate, place, category, "00:00:00"});
+                                }
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            //클릭 동시에 시작시간을 전에 활동 시작시간으로 update
+                            db.execSQL("update tb_timeline set endtime=? where endtime is NULL",
+                                    new String[]{formatTime});
+                            //현재 장소, 카테고리, 현재시간 insert
+                            db.execSQL("insert into tb_timeline (date, place, category, starttime) values (?,?,?,?)",
+                                    new String[]{formatDate, title, type, formatTime});
+                        }
+
+                        db.close();
+                    }
+                });
             }
         });
     }
-
+/*
     public void showAlertDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(GoogleMapActivity.this);
         LayoutInflater inflater = getLayoutInflater();
@@ -754,7 +849,7 @@ public class GoogleMapActivity extends AppCompatActivity
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
     }
-
+*/
     @Override
     public void onPlacesFinished() {
         //System.out.println("onPlacesFinished");
